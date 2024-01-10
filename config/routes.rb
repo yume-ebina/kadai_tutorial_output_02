19cd77ff18d649ac8a24a64ae20c3f283dba22da
yume-ebina <cityeventa@gmail.com>
2024-01-10 10:09:57 +0900

    finish 2-9

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
     root to: "home#index"
+
+    devise_scope :user do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
   end
