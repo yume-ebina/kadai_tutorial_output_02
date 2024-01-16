df109caed1c786c625516abbafba2171790733b2
yume-ebina <cityeventa@gmail.com>
2024-01-16 17:06:41 +0900

    finish 2-13

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,19 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// require("jquery3").start()
+// require("popper").start()
+// require("bootstrap-sprockets").start()
+
+// require rails-ujs
+// require activestorage
+// require turbolinks
+// require_tree .
+
+// Bootstrap4
+// require jquery3
+// require popper
+// require bootstrap-sprockets
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
