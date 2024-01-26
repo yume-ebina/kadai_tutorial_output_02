56a62a8cdbdb323db48936bc056c648c36e85764
yume-ebina <cityeventa@gmail.com>
2024-01-26 09:39:13 +0900

    fix:todo.tagsにタグを定義しました

--- app/models/todo_tag.rb
+++ app/models/todo_tag.rb
@@ -2,5 +2,5 @@ class TodoTag < ApplicationRecord
   belongs_to :todo
   belongs_to :tag
 
-  validates :todo_id, uniqueness => { :scope => :tag_id}
+  validates :todo_id, uniqueness: { :scope => :tag_id}
 end
